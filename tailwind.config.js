b5c6e249259149ff059cfdee6b91ffcdd72c0507
010binary <augustine32fg@gmail.com>
2025-01-22 00:40:43 +0100

    Add Tailwind CSS configuration and implement Map component

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/components/Map.jsx",
+    "./src/App.jsx",
+    "./src/main.jsx",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
